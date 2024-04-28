9e07525356b9821da581390c0b0817c12fc74c8d
Matt Stenson <matt.stenson@gmail.com>
2024-04-27 23:24:51 -0600

    add more docblock and typing

--- src/CloudflareAI.php
+++ src/CloudflareAI.php
@@ -11,7 +11,15 @@ class CloudflareAI
 
     }
 
-    public static function runModel(string $modelName, array $input)
+    /**
+     * Runs a Cloudflare AI model with the given input.
+     *
+     * @param string $modelName The name of the model to run.
+     * @param array<string, mixed> $input The input for the model.
+     * @return array<string, mixed> The output of the model.
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public static function runModel(string $modelName, array $input): array
     {
         $url = config('cloudflare-ai.api_url').'/accounts/'.config('cloudflare-ai.account_id').'/ai/run/@cf/'.$modelName;
         
@@ -27,15 +35,23 @@ class CloudflareAI
         }
     }
 
-    public static function runSpeechToText(string $modelName, $file)
+    /**
+     * Runs a Cloudflare AI speech to text model with the given audio file.
+     *
+     * @param string $modelName The name of the model to run.
+     * @param string $file The audio file to send to the model. This is the body of the file
+     * @return array<string, mixed> The output of the model.
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public static function runSpeechToText(string $modelName, $file): array
     {
-        $url = config('cloudflare-ai.api_url').'/accounts/'.config('cloudflare-ai.account_id').'/ai/run/@cf/'.$modelName;
-        
+        $url = config('cloudflare-ai.api_url') . '/accounts/' . config('cloudflare-ai.account_id') . '/ai/run/@cf/' . $modelName;
+
         try {
             $response = Http::withToken(config('cloudflare-ai.api_token'))
-                            ->contentType('application/octet-stream')
-                            ->withBody($file, 'application/octet-stream')
-                            ->post($url);
+                ->contentType('application/octet-stream')
+                ->withBody($file, 'application/octet-stream')
+                ->post($url);
 
             return $response->json();
         } catch (\Illuminate\Http\Client\RequestException $e) {
@@ -44,30 +60,56 @@ class CloudflareAI
         }
     }
 
-    public static function runImageClassification(string $modelName, $file)
-    {
-        $url = config('cloudflare-ai.api_url').'/accounts/'.config('cloudflare-ai.account_id').'/ai/run/@cf/'.$modelName;
+
+    /**
+     * Runs a Cloudflare AI image classification model with the given image file.
+     *
+     * @param string $modelName The name of the model to run.
+     * @param string $file The image file to send to the model. This is the body of the file.
+     * @return array<string, mixed> The output of the model.
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public static function runImageClassification(
+        string $modelName,
+        string $file
+    ): array {
+        $url = config('cloudflare-ai.api_url')
+            . '/accounts/'
+            . config('cloudflare-ai.account_id')
+            . '/ai/run/@cf/'
+            . $modelName;
         
         try {
             $response = Http::withToken(config('cloudflare-ai.api_token'))
-                            ->contentType('application/octet-stream')
-                            ->withBody($file, 'application/octet-stream')
-                            ->post($url);
+                ->contentType('application/octet-stream')
+                ->withBody($file, 'application/octet-stream')
+                ->post($url);
 
             return $response->json();
         } catch (\Illuminate\Http\Client\RequestException $e) {
             // Handle the error according to your application's needs
-            return response()->json(['error' => 'Request to Cloudflare API failed', 'details' => $e->getMessage()], 500);
+            return response()->json(
+                ['error' => 'Request to Cloudflare API failed', 'details' => $e->getMessage()],
+                500
+            );
         }
     }
 
-    public static function listFinetunes()
+
+    /**
+     * Get a list of finetunes.
+     *
+     * @return array<string, mixed>[] The list of finetunes
+     *
+     * @throws \Illuminate\Http\Client\RequestException
+     */
+    public static function listFinetunes(): array
     {
-        $url = config('cloudflare-ai.api_url').'/accounts/'.config('cloudflare-ai.account_id').'/ai/finetunes';
+        $url = config('cloudflare-ai.api_url') . '/accounts/' . config('cloudflare-ai.account_id') . '/ai/finetunes';
         
         try {
             $response = Http::withToken(config('cloudflare-ai.api_token'))
-                            ->get($url);
+                ->get($url);
 
             return $response->json();
         } catch (\Illuminate\Http\Client\RequestException $e) {
